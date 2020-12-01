770b8a138cf92bbabb43aaa30e787dafa3c8ce16
Daniel Uche <ddaniel@coursera.org>
2020-11-30 16:24:35 -0800

    update to read from env

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 const { DB_CONNECTION_STRING } = process.env;
 const client = new MongoClient(DB_CONNECTION_STRING,
